e03807d567481369f92a0aca02a4826b6bc5f35c
eoinfennessy <fennessyeoin@gmail.com>
2023-05-21 22:04:27 +0100

    Improve home page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <div class="columns is-vcentered content">
   <div class="column has-text-centered">
-    <img width="300" src="/camera.jpeg" alt="camera" />
+    <img src="/camera.jpeg" alt="camera" />
   </div>
-  <div class="column">
+  <div class="column has-text-centered">
     <h1 class="title">Welcome to ShutterSpotter</h1>
-    <p>A location-based social site for photographers</p>
+    <p class="subtitle">A location-based social site for photographers</p>
   </div>
 </div>
